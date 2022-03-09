11f630000adbd3541175e856d943f29aeec3f6a1
toranj-org <omidpoorali1992@gmail.com>
2022-03-09 20:50:22 +0330

    nextjs with typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div >
+      <main >
+        <h1 >
+          Toranj-UI
+        </h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home
